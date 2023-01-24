10060279006fb4ca224d6697c0184b66554dc9ff
Anton Chumak <Chumak7377@gmail.com>
2023-01-24 18:29:28 +0200

    Create models Task & Tag

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    create_date = models.DateField(auto_created=True)
+    deadline = models.DateField(auto_now_add=True)
+    marks = models.BooleanField()
+    tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["create_date", "-marks"]
+
+    def __str__(self):
+        return f"{self.content} {self.marks}"
