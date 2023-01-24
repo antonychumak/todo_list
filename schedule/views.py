3fd9cef298d3216d7a1c6cd9767f9dcdae2e01a0
Anton Chumak <Chumak7377@gmail.com>
2023-01-24 19:11:40 +0200

    Add views.py with the right paths

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import path
 
-# Create your views here.
+from schedule.urls import (
+    index,
+    TaskCreateView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("tasks/create", TaskCreateView.as_view, name="task-create"),
+    path("tags/create", TagCreateView.as_view, name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view, name="tag-update"),
+    path("tasks/<int:pk>/delete/", TagDeleteView.as_view, name="task-delete"),
+    ]
