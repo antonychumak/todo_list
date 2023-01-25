248518b84bfab0be544243bf116c5b0ab50400fe
Anton Chumak <Chumak7377@gmail.com>
2023-01-25 23:50:50 +0200

    Create tags_list_view

--- schedule/views.py
+++ schedule/views.py
@@ -11,13 +11,28 @@ from schedule.models import Task, Tag
 def index(request):
     context = {
         "todo_list": Task.objects.all(),
-        "tags": Tag.objects.all()
     }
 
     return render(request, "schedule/index.html", context=context)
 
 
+def tags_list_view(request):
+    context = {
+        "tags": Tag.objects.all(),
+    }
+
+    return render(request, "schedule/tags.html", context=context)
+
+
 class TaskCreateView(generic.CreateView):
+    pass
+
+
+class TaskUpdateView(generic.UpdateView):
+    pass
+
+
+class TaskDeleteView(generic.DeleteView):
     pass
 
 
