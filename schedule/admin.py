94d255333c05b93942c86a4b56be013cc5df07da
Anton Chumak <Chumak7377@gmail.com>
2023-01-25 23:48:09 +0200

    register Tag & Task to admin.py

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from schedule.models import Tag, Task
 
-admin.site.register(Tag)
-admin.site.register(Task)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = admin.ModelAdmin.list_display
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = admin.ModelAdmin.list_display
+    search_fields = ("deadline",)
+    prepopulated_fields = {"slug": ("tags",)}
