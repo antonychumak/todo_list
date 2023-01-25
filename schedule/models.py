4585599fb4689ebb258474311dc3d9908cc8e507
Anton Chumak <Chumak7377@gmail.com>
2023-01-25 22:46:29 +0200

    Add slug field to Tag & Task

--- schedule/models.py
+++ schedule/models.py
@@ -2,18 +2,23 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name="Tag name")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["name"]
+
 
 class Task(models.Model):
-    content = models.TextField()
-    create_date = models.DateTimeField(auto_created=True)
-    deadline = models.DateTimeField()
-    marks = models.BooleanField(default=False)
-    tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, verbose_name="Content")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name="Create date")
+    deadline = models.DateTimeField(auto_now=True, verbose_name="Deadline")
+    marks = models.BooleanField(default=False, verbose_name="Execution mark")
+    tags = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name="Tag name")
 
     class Meta:
         ordering = ["marks", "-create_date"]
