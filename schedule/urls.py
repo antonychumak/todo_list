a2931f16fc9d53945dfa96a93d4297828805739c
Anton Chumak <Chumak7377@gmail.com>
2023-01-25 23:50:12 +0200

    Update path to urls.py

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from .views import (
     index,
+    tags_list_view,
     TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
     TagCreateView,
     TagUpdateView,
     TagDeleteView
@@ -10,10 +13,13 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasks/create", TaskCreateView.as_view, name="task-create"),
+    path("tags/", tags_list_view, name="tags"),
     path("tags/create", TagCreateView.as_view, name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view, name="tag-update"),
-    path("tasks/<int:pk>/delete/", TagDeleteView.as_view, name="task-delete"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view, name="tag-delete"),
+    path("tasks/create", TaskCreateView.as_view, name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view, name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view, name="task-delete"),
     ]
 
 app_name = "schedule"
