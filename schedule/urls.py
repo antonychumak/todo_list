bb0e25cf9b55c20a42790cdbe8be2f09c7c2778b
Anton Chumak <Chumak7377@gmail.com>
2023-01-26 19:07:25 +0200

    Create path CRUD for Tag & Task

--- schedule/urls.py
+++ schedule/urls.py
@@ -14,12 +14,12 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", tags_list_view, name="tags"),
-    path("tags/create", TagCreateView.as_view, name="tag-create"),
-    path("tags/<int:pk>/update/", TagUpdateView.as_view, name="tag-update"),
-    path("tags/<int:pk>/delete/", TagDeleteView.as_view, name="tag-delete"),
-    path("tasks/create", TaskCreateView.as_view, name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUpdateView.as_view, name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view, name="task-delete"),
+    path("tags/create", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<slug:slug>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<slug:slug>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<slug:slug>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<slug:slug>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     ]
 
 app_name = "schedule"
