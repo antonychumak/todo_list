af6b0e5abe23fa2c76b8a5b71e6db92a8d1fde3d
Anton Chumak <Chumak7377@gmail.com>
2023-01-27 20:38:04 +0200

    remove secret key form settings.py

--- schedule/urls.py
+++ schedule/urls.py
@@ -14,12 +14,36 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", tags_list_view, name="tags"),
-    path("tags/create", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<slug:slug>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<slug:slug>/delete/", TagDeleteView.as_view(), name="tag-delete"),
-    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<slug:slug>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<slug:slug>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    ]
+    path(
+        "tags/create",
+        TagCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<slug:slug>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<slug:slug>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
+    ),
+    path(
+        "tasks/create",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<slug:slug>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<slug:slug>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+]
 
 app_name = "schedule"
