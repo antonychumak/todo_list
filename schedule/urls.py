7b4dad4e32d417aa64136c185c3cfd8c9831eb71
Anton Chumak <Chumak7377@gmail.com>
2023-01-30 17:02:53 +0200

    New migrations & fix page bugs to index.html

--- schedule/urls.py
+++ schedule/urls.py
@@ -15,17 +15,17 @@ urlpatterns = [
     path("", index, name="index"),
     path("tags/", tags_list_view, name="tags"),
     path(
-        "tags/create",
+        "tag/create",
         TagCreateView.as_view(),
         name="tag-create"
     ),
     path(
-        "tags/<slug:slug>/update/",
+        "tag/<slug:slug>/update/",
         TagUpdateView.as_view(),
         name="tag-update"
     ),
     path(
-        "tags/<slug:slug>/delete/",
+        "tag/<slug:slug>/delete/",
         TagDeleteView.as_view(),
         name="tag-delete"
     ),
