ec23b01f69fd6f72c568b5b5a1e5b367bb6c9c0d
Anton Chumak <Chumak7377@gmail.com>
2023-02-02 21:17:01 +0200

    remove required fields deadline & tags

--- schedule/models.py
+++ schedule/models.py
@@ -16,9 +16,9 @@ class Task(models.Model):
     content = models.TextField(blank=True, verbose_name="Content")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     create_date = models.DateTimeField(auto_now_add=True, verbose_name="Create date")
-    deadline = models.DateTimeField(auto_now=False, verbose_name="Deadline")
+    deadline = models.DateTimeField(auto_now=False, null=True, blank=True, verbose_name="Deadline")
     is_mark = models.BooleanField(default=False, verbose_name="Execution mark")
-    tags = models.ManyToManyField(Tag, related_name="tasks")
+    tags = models.ManyToManyField(Tag, blank=True, default=None, related_name="tasks")
 
     class Meta:
         ordering = ["is_mark", "-create_date"]
